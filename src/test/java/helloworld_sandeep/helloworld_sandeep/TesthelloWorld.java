1478068380dd0a434e6557d1a310f90fd3bfdc12
Sandeep <phalkesandeep25@gmail.com>
2020-05-26 01:31:47 +0400

    fourth

--- src/test/java/helloworld_sandeep/helloworld_sandeep/TesthelloWorld.java
+++ src/test/java/helloworld_sandeep/helloworld_sandeep/TesthelloWorld.java
@@ -11,6 +11,7 @@ public class TesthelloWorld {
 		
 		System.out.println("To check demo of jenkins with git commit 1");
 		System.out.println("To check demo of jenkins with git commit 2");
+		System.out.println("To check demo of jenkins with git commit 3");
 		
 //		System.setProperty("webdriver.chrome.driver", "W:\\Drivers\\chromedriver.exe");
 //		WebDriver driver = new ChromeDriver();
