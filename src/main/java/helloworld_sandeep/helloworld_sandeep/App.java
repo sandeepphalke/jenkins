656cb5c131364fb2ed93e468f65ec181784e2560
Sandeep <phalkesandeep25@gmail.com>
2020-05-26 01:20:39 +0400

    first

---
+++ src/main/java/helloworld_sandeep/helloworld_sandeep/App.java
@@ -0,0 +1,13 @@
+package helloworld_sandeep.helloworld_sandeep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/helloworld_sandeep/helloworld_sandeep/TesthelloWorld.java
@@ -0,0 +1,32 @@
+package helloworld_sandeep.helloworld_sandeep;
+
+import org.testng.annotations.Test;
+
+public class TesthelloWorld {
+	
+	@Test
+	public void testHelloWorld()
+	{
+		System.out.println("<<-------------------------------RUN STARTED------------------------------->>");
+		System.out.println("To check demo of jenkins with git commit");
+		System.out.println("To check demo of jenkins with git commit");
+		System.out.println("To check demo of jenkins with git commit");
+		System.out.println("To check demo of jenkins with git commit");
+		System.out.println("To check demo of jenkins with git commit");
+		System.out.println("To check demo of jenkins with git commit");
+		
+//		System.setProperty("webdriver.chrome.driver", "W:\\Drivers\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://www.flipkart.com/");
+//		System.out.println("The URL is : " + driver.getCurrentUrl());
+//		Thread.sleep(10000);
+//		driver.close();
+
+//		System.out.println("  ");
+//		System.out.println("  ");
+		System.out.println("<<-------------------------------RUN ENDED------------------------------->>");
+		
+	}
+
+}
