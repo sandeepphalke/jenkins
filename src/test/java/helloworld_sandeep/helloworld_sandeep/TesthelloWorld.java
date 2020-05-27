2490168406fc6c8778a0c138a51c6b26eda8956c
Sandeep <phalkesandeep25@gmail.com>
2020-05-27 15:02:43 +0400

    tenth

--- src/test/java/helloworld_sandeep/helloworld_sandeep/TesthelloWorld.java
+++ src/test/java/helloworld_sandeep/helloworld_sandeep/TesthelloWorld.java
@@ -1,31 +1,37 @@
 package helloworld_sandeep.helloworld_sandeep;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TesthelloWorld {
 	
+	
+	
+//	@BeforeClass
+//	public void preConditions()
+//	{
+//		System.out.println("<<-------------------------------RUN STARTED------------------------------->>");
+//		System.out.println("  ");
+//	}
+	
 	@Test
-	public void testHelloWorld()
+	public void testHelloWorld() throws InterruptedException
 	{
-		System.out.println("<<-------------------------------RUN STARTED------------------------------->>");
-		
-		System.out.println("To check demo of jenkins with git commit 1");
-		System.out.println("To check demo of jenkins with git commit 2");
-		System.out.println("To check demo of jenkins with git commit 3");
-		System.out.pr("To check demo of jenkins with git commit 4");
-		
-//		System.setProperty("webdriver.chrome.driver", "W:\\Drivers\\chromedriver.exe");
-//		WebDriver driver = new ChromeDriver();
-//		driver.manage().window().maximize();
-//		driver.get("https://www.flipkart.com/");
-//		System.out.println("The URL is : " + driver.getCurrentUrl());
-//		Thread.sleep(10000);
-//		driver.close();
-
-//		System.out.println("  ");
-//		System.out.println("  ");
-		System.out.println("<<-------------------------------RUN ENDED------------------------------->>");
-		
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "W:\\Drivers\\chromedriver.exe");
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		System.out.println("The URL is : " + driver.getCurrentUrl());
+		Thread.sleep(10000);	
 	}
+	
+//	@AfterClass
+//	public void postConditions()
+//	{
+//		driver.close();	
+//		System.out.println("  ");
+//		System.out.println("<<-------------------------------RUN ENDED------------------------------->>");
+//	}
 
 }
